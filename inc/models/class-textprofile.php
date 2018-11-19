bb33f19763a98122271783a4543d82034571fdc0
scott <scott@alley.co>
2018-11-18 19:40:50 -0500

    Add textprofile class for non-profile entries

---
+++ inc/models/class-textprofile.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Representation of a text-only profile.
+ *
+ * @package Byline_Manager
+ */
+
+namespace Byline_Manager\Models;
+
+class TextProfile {
+
+	/**
+	 * Not currently implemented, but useful to have defined.
+	 */
+	public $link = '';
+
+	/**
+	 * Instantiate a new Text Profile object
+	 *
+	 * Text Profiles are always fetched by static fetchers.
+	 *
+	 * @param array $atts Attributes for a text profile.
+	 */
+	private function __construct( array $atts ) {
+		$this->atts = $atts;
+		$this->display_name = $atts['text'] ?? false;
+	}
+
+	/**
+	 * Create a text profile object based on its attributes.
+	 *
+	 * @param array $atts Attributes for a text profile.
+	 * @return TextProfile|false
+	 */
+	public static function create( $atts ) {
+		// Require at least a text attribute.
+		if ( ! empty( $atts['text'] ) ) {
+			return new TextProfile( $atts );
+		}
+		return false;
+	}
+}
