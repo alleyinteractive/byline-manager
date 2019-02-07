8bbe2acd1818573f3c2a893b03ad6365e29efe18
Kaitlin Bolling <kaitlin@alley.co>
2019-02-07 10:08:20 -0500

    fix issue with lodash breaking media blocks

--- blocks/byline/client/src/components/authorFormatType/EditAuthorFormat.js
+++ blocks/byline/client/src/components/authorFormatType/EditAuthorFormat.js
@@ -11,7 +11,7 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import { find } from 'lodash';
+import { find } from 'lodash/find';
 
 import InlineAuthorUI from './InlineAuthorUI';
 
