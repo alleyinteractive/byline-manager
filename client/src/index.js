a49810185be86677ff72292d6ead340249071c87
Julio Braña Bouza <julio@alley.co>
2020-08-27 14:31:34 +0300

    add setting panel

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,29 @@
+import React from 'react';
+import BylineProfiles from 'components/BylineMetaBox/BylineProfiles';
 // Entry point for all shared JS.
-import initBylineMetaBox from './initBylineMetaBox';
-import initUserLinkMetaBox from './initUserLinkMetaBox';
+// import initBylineMetaBox from './initBylineMetaBox';
+// import initUserLinkMetaBox from './initUserLinkMetaBox';
 
 import './styles/styles.scss';
 
-if (document.getElementById('byline-manager-metabox-root')) {
-  initBylineMetaBox();
-} else if (document.getElementById('byline-manager-user-link-root')) {
-  initUserLinkMetaBox();
-}
+// if (document.getElementById('byline-manager-metabox-root')) {
+//   initBylineMetaBox();
+// } else if (document.getElementById('byline-manager-user-link-root')) {
+//   initUserLinkMetaBox();
+// }
+
+const { registerPlugin } = wp.plugins;
+const { PluginDocumentSettingPanel } = wp.editPost;
+
+const BylineSetting = () => (
+  <PluginDocumentSettingPanel
+    name="byline"
+    title="Byline"
+    className="byline"
+  >
+    <BylineProfiles />
+  </PluginDocumentSettingPanel>
+
+);
+
+registerPlugin('byline-setting', { render: BylineSetting });
