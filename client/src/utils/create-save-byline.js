21ea10ab40aeddeab74d8dd48e5886881adcb492
jomurgel <dev@jomurgel.com>
2022-02-22 15:25:18 -0700

    moves create-save-byline

---
+++ client/src/utils/create-save-byline.js
@@ -0,0 +1,38 @@
+import transformHydratedProfiles from './transform-hydrate-profiles';
+
+/**
+ * Create a function to save bylines to meta.
+ */
+const createSaveByline = (dispatch) => (profiles) => {
+  if (! profiles.length) {
+    return profiles;
+  }
+
+  const preparedProfiles = transformHydratedProfiles(profiles);
+
+  // Save byline tax term relationships.
+  const termBylines = preparedProfiles.filter(
+    (value) => (value.type && 'byline_id' === value.type)
+  );
+
+  if (termBylines.length) {
+    dispatch('core/editor').editPost({
+      byline: [...termBylines.map((item) => item.atts.term_id)],
+    });
+  } else {
+    dispatch('core/editor').editPost({
+      byline: [],
+    });
+  }
+
+  // Save profile metadata.
+  return dispatch('core/editor').editPost({
+    meta: {
+      byline: {
+        profiles: preparedProfiles,
+      },
+    },
+  });
+};
+
+export default createSaveByline;
