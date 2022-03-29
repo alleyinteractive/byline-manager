98401fad7f88063031e2abed51b9dae89540f082
Joe McGill <joemcgill@gmail.com>
2022-03-28 20:07:28 -0500

    Add bylineText field to graphql
    This modifies the previous approach for supplying byline data via GraphQL so that clients can query for both a simple text version of the byline and the underlying profile data for constructing custom interfaces that require more formatting.
    
    A new `Byline` object type is now registered as a `byline` field on the `NodeWithAuthor` interface, which has two fields:
    
    - `bylineText`: A formatted string which supports three formats:
    	- 'text': The default option, text only. `get_the_byline()`
    	- 'post_links': String with links to profile post pages. `get_the_byline_posts_links()`
    	- 'links': String with links to profile web links, if present. `get_the_byline_links()`
    - `profiles`: The raw data for profiles, as either a `Profile` type or a `TextProfile`.
    
    This also adds support for passing a post to each of the byline template tags as either a `WP_Post` object or post ID.

--- inc/graphql.php
+++ inc/graphql.php
@@ -47,6 +47,81 @@ function register_byline_types() {
 		]
 	);
 
+	register_graphql_enum_type(
+		'BylineTextEnum',
+		[
+			'description' => __( 'Byline format', 'byline-manager' ),
+			'values'      => [
+				'TEXT'       => [
+					'value' => 'text',
+				],
+				'LINKS'      => [
+					'value' => 'links',
+				],
+				'POST_LINKS' => [
+					'value' => 'post_links',
+				],
+			],
+		]
+	);
+
+	register_graphql_object_type(
+		'Byline',
+		[
+			'description' => __( 'Byline manager byline', 'byline-manager' ),
+			'fields'      => [
+				'bylineText'      => [
+					'args'        => [
+						'format' => [
+							'type'        => 'BylineTextEnum',
+							'description' => __( 'Choose the byline format.', 'byline-manager' ),
+						],
+					],
+					'type'        => 'String',
+					'description' => __( 'Byline text', 'byline-manager' ),
+					'resolve'     => function( WPGraphQL\Model\Post $post, array $args ) {
+						switch ( $args['format'] ) {
+							case 'links':
+								$byline = get_the_byline_links( $post->ID );
+								break;
+
+							case 'post_links':
+								$byline = get_the_byline_posts_links( $post->ID );
+								break;
+
+							case 'text':
+							default:
+								$byline = get_the_byline( $post->ID );
+								break;
+						}
+
+						return $byline;
+					},
+				],
+				'profiles' => [
+					'type'        => [ 'list_of' => 'ProfileTypes' ],
+					'description' => __( 'Byline profiles', 'byline-manager' ),
+					'resolve'     => function ( WPGraphQL\Model\Post $post ) {
+						$byline_data = get_post_meta( $post->ID, 'byline', true );
+
+						$bylines = array_map(
+							function( $profile ) {
+								if ( 'byline_id' === $profile['type'] ) {
+									return new WPGraphQL\Model\Post( get_post( $profile['atts']['post_id'] ) );
+								}
+
+								return $profile['atts'];
+							},
+							$byline_data['profiles'],
+						);
+
+						return $bylines;
+					},
+				],
+			],
+		]
+	);
+
 	/*
 	 * Register byline fields on nodes that support authors, which is also
 	 * the check byline manager uses to determine if a post supports bylines.
@@ -57,23 +132,10 @@ function register_byline_types() {
 		'NodeWithAuthor',
 		'byline',
 		[
-			'type'        => [ 'list_of' => 'ProfileTypes' ],
+			'type'        => 'Byline',
 			'description' => __( 'Byline profiles', 'byline-manager' ),
 			'resolve'     => function ( WPGraphQL\Model\Post $post ) {
-				$byline_data = get_post_meta( $post->ID, 'byline', true );
-
-				$bylines = array_map(
-					function( $profile ) {
-						if ( 'byline_id' === $profile['type'] ) {
-							return new WPGraphQL\Model\Post( get_post( $profile['atts']['post_id'] ) );
-						}
-
-						return $profile['atts'];
-					},
-					$byline_data['profiles'],
-				);
-
-				return $bylines;
+				return $post;
 			},
 		]
 	);
