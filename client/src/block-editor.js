2356f7e647381633c0385c834d5e6aebfdcd8bbb
Kaitlin Bolling <kaitlin@alley.co>
2022-03-30 16:32:58 -0400

    add divider above byline slot

--- client/src/block-editor.js
+++ client/src/block-editor.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
+import { __experimentalDivider as Divider } from '@wordpress/components';
 
 // Components.
 import BylineSlot from 'components/byline-slot';
@@ -15,6 +16,7 @@ import './styles/styles.scss';
 const BylineSlotFill = () => (
   <PluginPostStatusInfo>
     <div style={{ width: '100%' }}>
+      <Divider />
       <p>
         <strong>{__('Byline', 'byline-manager')}</strong>
       </p>
