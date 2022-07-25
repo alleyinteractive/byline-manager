a3f86f74fc81514ee774a053fd58e0fa8c96e392
David Herrera <david@alley.co>
2022-07-25 20:43:49 +0000

    Merge pull request #72 from alleyinteractive/fix/save-no-bylines Allow saving an empty set of bylines

---
+++ client/build/blockEditor.9f28eba1.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '9f28eba11895483f1404ce28b3758817');
