5839cbfb40d6694b272a723829f43de1f9759013
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2023-08-18 15:59:52 +0000

    Merge pull request #146 from alleyinteractive/feature/empty-list Hide the `Bylinelist` if profiles are empty

--- client/build/blockEditor.61ea8902.bundle.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '61ea89023ed63a68b2162af08549eb15');
