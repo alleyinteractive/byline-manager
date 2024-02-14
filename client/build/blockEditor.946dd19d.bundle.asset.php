b19fa49212bf461bc9b3dfedbd6bc92f07e7b238
kevinfodness <kevinfodness@users.noreply.github.com>
2024-02-14 23:16:18 +0000

    Built changes for v0.2.7 from production a2c293f99d5b0e8262bd16123aa47c3fd90690e6

---
+++ client/build/blockEditor.946dd19d.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'a635810d34d9b7399d0e');
---
+++ client/build/main.8a4a0a5b.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-url'), 'version' => '457b6e9d1d0a13d54da4');
