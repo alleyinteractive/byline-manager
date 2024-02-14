8c96fc2e69bbcec65cd34a0853e73d8d2eea788c
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2024-02-14 23:01:50 +0000

    Merge pull request #261 from alleyinteractive/hotfix/createroot-error Fix incorrect usage of ReactDOM.createRoot()

--- client/build/main.76a20f6f.bundle.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-url'), 'version' => '1b18c9f02f7599b52175');
---
+++ client/build/main.ff569f78.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-url'), 'version' => '43b6eeab8f1c1ae2eca1');
