7355a0f53e7828af6a1e5cf5d787bd2aecec57d4
mboynes <mboynes@users.noreply.github.com>
2024-02-28 01:04:37 +0000

    Built changes for v0.2.8 from production 7380fc002a7ff925ee29416e5c46cc7c34924a0a

---
+++ client/build/blockEditor.6113d243.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'e113d8f2cf631ece0a71');
