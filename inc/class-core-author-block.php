ef36854ecc40bee89d1ad857ac93b556273669ea
Ron Pasillas <ron@alley.com>
2024-07-05 11:06:20 -0700

    PHPCS fix.

--- inc/class-core-author-block.php
+++ inc/class-core-author-block.php
@@ -59,7 +59,7 @@ class Core_Author_Block {
 		if ( ! empty( $post ) && property_exists( $post, 'ID' ) ) {
 			// Get the byline meta and set it to our property.
 			$byline         = get_post_meta( $post->ID, 'byline', true );
-			$this::$bylines = ! empty ( $byline ) ? $byline : [];
+			$this::$bylines = ! empty( $byline ) ? $byline : [];
 		}
 
 		add_filter( 'render_block_core/post-author', [ $this, 'append_and_filter_post_author_blocks' ], 10, 2 );
