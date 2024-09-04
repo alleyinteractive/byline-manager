2db3258c7b00bd30ed5800afe0c1e8d1aee9c671
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2024-09-04 16:22:30 +0000

    Merge pull request #401 from alleyinteractive/feature/TECH-127/add-postpicker Feature/tech 127/add postpicker

---
+++ client/build/blockEditor.82499dc4.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '328dcb6a24e9bb29f649');
