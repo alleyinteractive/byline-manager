f2671c5afc3450f45a6092bcc329f44330f18ab0
Sean Fisher <srtfisher@gmail.com>
2025-03-06 15:22:51 -0500

    Bumping to php 8.1, aligning CI workflows with create-wordpress-plugin

--- byline-manager.php
+++ byline-manager.php
@@ -8,9 +8,9 @@
  * Text Domain:     byline-manager
  * Domain Path:     /languages
  * Version:         0.6.0
- * Requires WP:     5.9
- * Requires PHP:    8.0
- * Tested up to:    6.4
+ * Requires WP:     6.3
+ * Requires PHP:    8.1
+ * Tested up to:    6.5
  *
  * @package Byline_Manager
  */
