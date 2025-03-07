0eec1c56ec88901bab1c0187b6fc21698be0b112
Sean Fisher <srtfisher@gmail.com>
2025-03-07 09:52:42 -0500

    Bump version

--- byline-manager.php
+++ byline-manager.php
@@ -7,7 +7,7 @@
  * Author URI:      https://alley.com
  * Text Domain:     byline-manager
  * Domain Path:     /languages
- * Version:         0.6.0
+ * Version:         0.6.1
  * Requires WP:     6.3
  * Requires PHP:    8.1
  * Tested up to:    6.7
