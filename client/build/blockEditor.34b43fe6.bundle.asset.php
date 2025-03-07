b3a32f56e2fdb8ac6f4907e589d32d633626e966
srtfisher <srtfisher@users.noreply.github.com>
2025-03-07 14:55:56 +0000

    Built changes for v0.6.1 from production ee92088f94b69c6057afe6000eae31274df159e8

---
+++ client/build/blockEditor.34b43fe6.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '39ff2f25d6e25f0f7f15');
