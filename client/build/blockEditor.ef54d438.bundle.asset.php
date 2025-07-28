fd243fcb44e987e3d735a332dd0dc9931704870e
scottnelle <scottnelle@users.noreply.github.com>
2025-07-28 19:56:52 +0000

    Built changes for v0.6.2 from production 13e2813150d3b31ba3cf31e64704d46b21305e68

---
+++ client/build/blockEditor.ef54d438.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => 'a0095d6e98a2ded7824a');
