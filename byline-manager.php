657e83cc3b4ae48b52cbb8a9e5aa26fced1c5521
Scott Nelle <scott@alley.co>
2025-07-30 09:10:50 -0400

    Update Block Editor tools and release 0.6.3 (#510)
    * upgrade block-editor-tools to 0.13.0
    
    * increment plugin version

--- byline-manager.php
+++ byline-manager.php
@@ -7,7 +7,7 @@
  * Author URI:      https://alley.com
  * Text Domain:     byline-manager
  * Domain Path:     /languages
- * Version:         0.6.2
+ * Version:         0.6.3
  * Requires WP:     6.3
  * Requires PHP:    8.1
  * Tested up to:    6.8
