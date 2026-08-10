87bfbf098af25ed57a46e935439e1def43378f76
dlh01 <dlh01@users.noreply.github.com>
2026-08-10 14:55:19 +0000

    Built changes for v0.7.0 from production d8ae0746639b54bd88d2503a779718878c13ed40

---
+++ client/build/blockEditor.bea497b5.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-url'), 'version' => '6b4622906ae7bf7f1dab');
---
+++ client/build/main.d7ad5548.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-url'), 'version' => '3f331bad8ef95c07bc4b');
